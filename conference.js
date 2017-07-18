781fb1da8566af88213396e914754f188f02a555
Thiago Andrade <thiagoh@gmail.com>
2017-07-17 22:13:43 -0400

    pipeline almost done

--- conference.js
+++ conference.js
@@ -28,34 +28,44 @@ new Promise(function(resolve, reject) {
   });
 
   fs.readFile('./bin/Conference.bin', function(err, data) {
-    if (err) { console.error(err); throw error; }
+    if (err) { console.error(err); throw err; }
     // console.log(data.toString());
     countAndCaller(0, data.toString());
   });
 
   fs.readFile('./bin/Conference.abi', function(err, data) {
-    if (err) { console.error(err); throw error; }
+    if (err) { console.error(err); throw err; }
     // console.log(data.toString());
     countAndCaller(1, data.toString());
   });
 });
 
-if (false) {
+var contract = web3.eth.contract(abi);
 
-  var previouslyDeployedAddress = '0x8373f6d76caa6a27247f9dc3a898976ed445a7da';
+var mainAccount = web3.eth.accounts[0];
+console.log('Deployer account: ' + mainAccount);
 
-  var contract = web3.eth.contract(abi);
+var argv = minimist(process.argv.slice(2));
+// console.log(argv);
+var contractAddressFile = './bin/Conference.address';
 
-  var mainAccount = web3.eth.accounts[0];
-  console.log('Deployer account: ' + mainAccount);
+new Promise(function(resolve, reject) {
 
-  var argv = minimist(process.argv.slice(2));
-  // console.log(argv);
+    fs.readFile(contractAddressFile, function(err, data) {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      // console.log(data.toString());
+      resolve(data.toString());
+    });
 
-  new Promise(function(resolve, reject) {
+  })
+  .then(function(previouslyDeployedAddress) {
 
-      if (argv.submit === true || argv.submit === 'true') {
+    if (argv.submit === true || argv.submit === 'true') {
 
+      return new Promise(function(resolve, reject) {
         contract.new({
             from: mainAccount,
             data: '0x' + code,
@@ -69,175 +79,186 @@ if (false) {
 
               if (typeof contract.address !== 'undefined') {
                 console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-                resolve(contract.address);
+
+                var write = function() {
+                  fs.writeFile(contractAddressFile, contract.address);
+                  resolve(contract.address);
+                };
+
+                fs.exists(contractAddressFile, function(exist) {
+                  if (exist) {
+                    fs.unlink(contractAddressFile, function() {
+                      write();
+                    });
+                  } else {
+                    write();
+                  }
+                });
               }
             }
           });
-      } else {
-
-        resolve(previouslyDeployedAddress);
-      }
-    })
-    .then(function(address) {
-
-        var conference = web3.eth.contract(abi).at(address);
-
-        var event = conference.allEvents();
-        event.watch(function(error, result) {
-          if (error) {
-            console.log("Error: " + error);
-          } else {
-            console.log("Event: " + result.event);
-
-            if (result.event === 'Deposit') {
-              // console.log('    ', result.args);
-              console.log('DEPOSIT:');
-              console.log('    ', 'result.args._amount', result.args._amount);
-              console.log('    ', 'conference.quota()', conference.quota());
-              console.log('    ', 'conference.numRegistrants()', conference.numRegistrants());
-              console.log('    ', 'conference.totalAmount()', conference.totalAmount());
-            } else if (result.event === 'Error') {
-              console.error('ERROR: ', result.args.message);
-              console.error('    ', '_from', result.args._from);
-              console.error('    ', '_to', result.args._to);
-              console.error('    ', '_amount', result.args._amount);
-            } else if (result.event === 'Info') {
-              console.log('INFO: ', result.args.message);
-              console.log('    ', '_from', result.args._from);
-              console.log('    ', '_to', result.args._to);
-              console.log('    ', '_amount', result.args._amount);
-            }
-
-          }
-        });
-
-        console.log('conference.address', conference.address);
-        console.log('conference.code', web3.eth.getCode(conference.address).substring(0, 80) + '...');
-
-        var stdin = process.openStdin();
-
-        stdin.on("data", function(d) {
-
-          var line = d.toString().trim();
-          var account;
-          var ticketPrice = web3.toWei(23, 'ether');
-          var conferenceArgv = minimist(line.split(' '));
-          console.log(conferenceArgv);
-
-          if (typeof conferenceArgv.eval === 'string' || conferenceArgv.eval === true) {
-
-            // console.log('conference.conferenceBalance()', conference.conferenceBalance());
-
-            var cmd = '';
-
-            if (typeof conferenceArgv.eval === 'string' && typeof conferenceArgv.cmd === 'undefined') {
-              cmd = conferenceArgv.eval.trim().replace(/^'(.*)/, '$1').replace(/(.*)'$/, '$1');
-            } else if (typeof conferenceArgv.cmd === 'undefined') {
-              console.error('cmd not defined. Please use --cmd to set the source cmd');
-              return;
-            }
-
-            // console.log(cmd);
-
-            try {
-
-              eval(cmd);
-
-            } catch (error) {
-              console.error(error);
-            }
-
-          } else if (conferenceArgv.info === true) {
-
-            console.log('conference.address', conference.address);
-            console.log('conference.organizer()', conference.organizer());
-            console.log('getBalance(conference.address)', web3.eth.getBalance(conference.address).toNumber());
-            console.log('getBalance(conference.organizer())', web3.eth.getBalance(conference.organizer()).toNumber());
-
-            console.log('conference.quota()', conference.quota());
-            console.log('conference.numRegistrants()', conference.numRegistrants());
-            console.log('conference.totalAmount()', conference.totalAmount());
-
-          } else if (typeof conferenceArgv.sendFunds === 'string' || conferenceArgv.sendFunds === true) {
-
-            console.log('Sending funds to the organizer');
-            console.log('The organizer ', conference.organizer(), ' will receive ', conference.totalAmount());
-
-            account = '';
-
-            if (typeof conferenceArgv.sendFunds === 'string' && typeof conferenceArgv.account === 'undefined') {
-              account = conferenceArgv.sendFunds.replace(/'/g, '');
-            } else if (typeof conferenceArgv.account === 'undefined') {
-              console.error('Account not defined. Please use --account to set the source account');
-              return;
-            } else if (typeof conferenceArgv.account !== 'string') {
-              console.error('Account should be a string. Please use --account \'account id\' to set the source account');
-              return;
-            }
-
-            console.log('Sending funds to the organizer from account ' + account);
-
-            try {
-
-              conference.sendFunds.call({
-                from: account,
-                value: ticketPrice
-              }, function(err, result) {
-
-                if (err) {
-                  console.error(err);
-                  return;
-                }
-
-                // console.log(result.constructor);
-
-                if (result.valueOf() < 0) {
-                  console.error('Sender is NOT the same as the organizer');
-                  return;
-                }
-
-                conference.sendFunds({ from: account, value: ticketPrice });
-              });
-
-            } catch (error) {
-              console.error(error);
-            }
-
-          } else if (typeof conferenceArgv.buy === 'string' || conferenceArgv.buy === true) {
-
-            account = '';
-
-            if (typeof conferenceArgv.buy === 'string' && typeof conferenceArgv.account === 'undefined') {
-              account = conferenceArgv.buy.replace(/'/g, '');
-            } else if (typeof conferenceArgv.account === 'undefined') {
-              console.error('Account not defined. Please use --account to set the source account');
-              return;
-            } else if (typeof conferenceArgv.account !== 'string') {
-              console.error('Account should be a string. Please use --account \'account id\' to set the source account');
-              return;
-            }
-
-            console.log('Buying ticket from account ' + account);
-
-            try {
-
-              conference.buyTicket({ from: account, value: ticketPrice });
-
-            } catch (error) {
-              console.error(error);
-            }
-
-          } else if (conferenceArgv._[0] === '') {
-            // nop
-          } else {
-            console.log('No action linked to this argument');
-          }
-
-        });
-      },
-      function(error) {
-        console.error(error);
       });
 
+    } else {
+      return previouslyDeployedAddress;
+    }
 
-}
+  })
+  .then(function(address) {
+
+    var conference = web3.eth.contract(abi).at(address);
+
+    var event = conference.allEvents();
+    event.watch(function(error, result) {
+      if (error) {
+        console.log("Error: " + error);
+      } else {
+        console.log("Event: " + result.event);
+
+        if (result.event === 'Deposit') {
+          // console.log('    ', result.args);
+          console.log('DEPOSIT:');
+          console.log('    ', 'result.args._amount', result.args._amount);
+          console.log('    ', 'conference.quota()', conference.quota());
+          console.log('    ', 'conference.numRegistrants()', conference.numRegistrants());
+          console.log('    ', 'conference.totalAmount()', conference.totalAmount());
+        } else if (result.event === 'Error') {
+          console.error('ERROR: ', result.args.message);
+          console.error('    ', '_from', result.args._from);
+          console.error('    ', '_to', result.args._to);
+          console.error('    ', '_amount', result.args._amount);
+        } else if (result.event === 'Info') {
+          console.log('INFO: ', result.args.message);
+          console.log('    ', '_from', result.args._from);
+          console.log('    ', '_to', result.args._to);
+          console.log('    ', '_amount', result.args._amount);
+        }
+      }
+    });
+
+    console.log('conference.address', conference.address);
+    console.log('conference.code', web3.eth.getCode(conference.address).substring(0, 80) + '...');
+
+    var stdin = process.openStdin();
+
+    stdin.on("data", function(d) {
+
+      var line = d.toString().trim();
+      var account;
+      var ticketPrice = web3.toWei(23, 'ether');
+      var conferenceArgv = minimist(line.split(' '));
+      console.log(conferenceArgv);
+
+      if (typeof conferenceArgv.eval === 'string' || conferenceArgv.eval === true) {
+
+        // console.log('conference.conferenceBalance()', conference.conferenceBalance());
+
+        var cmd = '';
+
+        if (typeof conferenceArgv.eval === 'string' && typeof conferenceArgv.cmd === 'undefined') {
+          cmd = conferenceArgv.eval.trim().replace(/^'(.*)/, '$1').replace(/(.*)'$/, '$1');
+        } else if (typeof conferenceArgv.cmd === 'undefined') {
+          console.error('cmd not defined. Please use --cmd to set the source cmd');
+          return;
+        }
+
+        // console.log(cmd);
+
+        try {
+
+          eval(cmd);
+
+        } catch (error) {
+          console.error(error);
+        }
+
+      } else if (conferenceArgv.info === true) {
+
+        console.log('conference.address', conference.address);
+        console.log('conference.organizer()', conference.organizer());
+        console.log('getBalance(conference.address)', web3.eth.getBalance(conference.address).toNumber());
+        console.log('getBalance(conference.organizer())', web3.eth.getBalance(conference.organizer()).toNumber());
+
+        console.log('conference.quota()', conference.quota());
+        console.log('conference.numRegistrants()', conference.numRegistrants());
+        console.log('conference.totalAmount()', conference.totalAmount());
+
+      } else if (typeof conferenceArgv.sendFunds === 'string' || conferenceArgv.sendFunds === true) {
+
+        console.log('Sending funds to the organizer');
+        console.log('The organizer ', conference.organizer(), ' will receive ', conference.totalAmount());
+
+        account = '';
+
+        if (typeof conferenceArgv.sendFunds === 'string' && typeof conferenceArgv.account === 'undefined') {
+          account = conferenceArgv.sendFunds.replace(/'/g, '');
+        } else if (typeof conferenceArgv.account === 'undefined') {
+          console.error('Account not defined. Please use --account to set the source account');
+          return;
+        } else if (typeof conferenceArgv.account !== 'string') {
+          console.error('Account should be a string. Please use --account \'account id\' to set the source account');
+          return;
+        }
+
+        console.log('Sending funds to the organizer from account ' + account);
+
+        try {
+
+          conference.sendFunds.call({
+            from: account,
+            value: ticketPrice
+          }, function(err, result) {
+
+            if (err) {
+              console.error(err);
+              return;
+            }
+
+            // console.log(result.constructor);
+
+            if (result.valueOf() < 0) {
+              console.error('Sender is NOT the same as the organizer');
+              return;
+            }
+
+            conference.sendFunds({ from: account, value: ticketPrice });
+          });
+
+        } catch (error) {
+          console.error(error);
+        }
+
+      } else if (typeof conferenceArgv.buy === 'string' || conferenceArgv.buy === true) {
+
+        account = '';
+
+        if (typeof conferenceArgv.buy === 'string' && typeof conferenceArgv.account === 'undefined') {
+          account = conferenceArgv.buy.replace(/'/g, '');
+        } else if (typeof conferenceArgv.account === 'undefined') {
+          console.error('Account not defined. Please use --account to set the source account');
+          return;
+        } else if (typeof conferenceArgv.account !== 'string') {
+          console.error('Account should be a string. Please use --account \'account id\' to set the source account');
+          return;
+        }
+
+        console.log('Buying ticket from account ' + account);
+
+        try {
+
+          conference.buyTicket({ from: account, value: ticketPrice });
+
+        } catch (error) {
+          console.error(error);
+        }
+
+      } else if (conferenceArgv._[0] === '') {
+        // nop
+      } else {
+        console.log('No action linked to this argument');
+      }
+    });
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
